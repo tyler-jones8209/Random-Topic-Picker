92cd758704fee71d7940e28cc392417360253000
Tyler Jones <tdjones1980@yahoo.com>
2026-01-15 15:36:06 -0600

    Started topics_registry file

---
+++ topics_registry.py
@@ -0,0 +1,14 @@
+TOPICS = {
+    "shamanism": {
+        "name": "Shamanism",
+        "description": "Animistic religious practices involving spirit communication",
+        "tags": ["religion", "anthropology"],
+        "origins": ["korean", "siberian"]
+    },
+    "hangul": {
+        "name": "Hangul",
+        "description": "The Korean writing system",
+        "tags": ["language", "writing"],
+        "origins": ["korean"]
+    }
+}
