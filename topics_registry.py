08ee4c034781794b53f674da56ed9f2069a62930
Tyler Jones <tdjones1980@yahoo.com>
2026-01-15 21:38:28 -0600

    Updating

--- topics_registry.py
+++ topics_registry.py
@@ -1,14 +1,53 @@
+''' EXAMPLE STRUCTURE
+
+    "": {
+        "name": "",
+        "description": "",
+        "tags": [],
+        "origins": []
+    },
+'''
+
 TOPICS = {
+
+    # RELIGION
+
+    "christianity": {
+        "name": "Christianity",
+        "description": "Monothistic religion focused around a Holy God and his bastard son Jesus Christ",
+        "tags": ["religion", "anthropology", "colonism"],
+        "origins": "roman judea"
+    },
+
     "shamanism": {
         "name": "Shamanism",
         "description": "Animistic religious practices involving spirit communication",
         "tags": ["religion", "anthropology"],
         "origins": ["korean", "siberian"]
     },
+
+    "shinto": {
+        "name": "Shinto (神道)",
+        "description": "A polytheistic and animistic religion that revolves around supernatural entities called kami (神)",
+        "tags": ["religion", "anthropology"],
+        "origins": "japan"
+    },
+
+    # WRITTEN LANGUAGE
+
     "hangul": {
         "name": "Hangul",
         "description": "The Korean writing system",
         "tags": ["language", "writing"],
         "origins": ["korean"]
-    }
+    },
+
+    # SPOKEN LANGUAGE
+
+        "hangugeo": {
+        "name": "Hangugeo (The Korean Language)",
+        "description": "A term used in Korea to represent the Korean language as a whole - 한국어",
+        "tags": ["language", "speaking"],
+        "origins": ["korean"]
+    },
 }
